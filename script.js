e53df0b20d99fff7bc0978fb6ebe563f8fe8fd37
Scott Nasman <scottnasman@gmail.com>
2021-03-27 13:02:42 -0600

    Created the script to duplicate the box as html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let myLibrary = [
     {
-        title: Hyperion,
-        author: Dan Simmons,
+        title: 'Hyperion',
+        author: 'Dan Simmons',
         pages: 482
     }
 ];
@@ -21,10 +21,38 @@ const inputForm = document.querySelector('[data-input-form]')
 
 function addBookToLibrary() {
     for (let i = 0; i < myLibrary.length; i++) {
-        let book = document.createElement('div')
+        const book = document.createElement('div')
         book.classList.add('book')
+        library.appendChild(book)
+
+        const bookTitle = document.createElement('h2')
+        bookTitle.textContent = myLibrary[i].title
+        book.appendChild(bookTitle)
+
+        const bookAuthor = document.createElement('p')
+        bookAuthor.textContent = myLibrary[i].author
+        book.appendChild(bookAuthor)
+        
+        const bookPages = document.createElement('p')
+        bookPages.textContent = myLibrary[i].pages + ' pages'
+        book.appendChild(bookPages)
+
+        const bookButtons = document.createElement('div')
+        bookButtons.classList.add('book-btns')
+        book.appendChild(bookButtons)
+
+        const readBook = document.createElement('button')
+        readBook.classList.add('read')
+        readBook.textContent = 'Read'
+        bookButtons.appendChild(readBook)
+
+        const deleteBook = document.createElement('button')
+        deleteBook.classList.add('delete')
+        deleteBook.textContent = 'Delete'
+        bookButtons.appendChild(deleteBook)
     }
 }
+addBookToLibrary()
 
 newBookButton.addEventListener('click', () => {
     inputForm.style.visibility = 'visible'
